6be686c8d26253a104c064bc584b3652b8241324
Foreverflying <foreverflying@live.cn>
2018-10-08 08:43:54 +1300

    finish step 4

--- public/spreadsheet.js
+++ public/spreadsheet.js
@@ -1,5 +1,12 @@
 
 var sheetData = {}
+var focusX = 1
+var focusY = 1
+
+const ROW_COUNT = 100
+const COLUMN_COUNT = 100
+
+var gridName, gridValue, sideBar, rowNumber, sheetHeader, sheetContent
 
 function getColumnName(x) {
     var arr = []
@@ -19,7 +26,79 @@ function getGridName(x, y) {
     return getColumnName(x) + y
 }
 
-function setSheetBodyHeight(sideBar, sheetContent) {
+function getGridValue(x, y) {
+    var data = sheetData["v" + x + "_" + y]
+    return data ? data.value : ""
+}
+
+function setGridValue(x, y, val) {
+    var key = "v" + x + "_" + y
+    var data = sheetData[key]
+    if (data && val !== "") {
+        data.value = val
+    } else {
+        sheetData[key] = { value: val }
+    }
+}
+
+function getGridByXY(x, y) {
+    return sheetContent.children().eq(y - 1).children().eq(x - 1)
+}
+
+function getRowNumberGridByY(y) {
+    return $("#r" + y)
+}
+
+function getColumnGridByX(x) {
+    return $("#c" + x)
+}
+
+function commitEdit() {
+    var val = gridValue.val()
+    setGridValue(focusX, focusY, val)
+    var grid = getGridByXY(focusX, focusY)
+    grid.text(val)
+}
+
+function switchFocusGrid(x, y) {
+    commitEdit()
+    var focusColumn = getColumnGridByX(focusX)
+    focusColumn.removeClass("gridFocus")
+    var focusRowNumber = getRowNumberGridByY(focusY)
+    focusRowNumber.removeClass("gridFocus")
+    var focusGrid = getGridByXY(focusX, focusY)
+    focusGrid.removeClass("gridFocus")
+    focusX = x
+    focusY = y
+    var focusGrid = getGridByXY(focusX, focusY)
+    focusGrid.addClass("gridFocus")
+    var focusRowNumber = getRowNumberGridByY(focusY)
+    focusRowNumber.addClass("gridFocus")
+    var focusColumn = getColumnGridByX(focusX)
+    focusColumn.addClass("gridFocus")
+    var name = getGridName(x, y)
+    gridName.val(name)
+    gridValue.val(getGridValue(x, y))
+    gridValue.focus()
+}
+
+function switchNeighborGridUp(x, y) {
+    switchFocusGrid(x, y === 1 ? y : y - 1)
+}
+
+function switchNeighborGridDown(x, y) {
+    switchFocusGrid(x, y === 100 ? y : y + 1)
+}
+
+function switchNeighborGridLeft(x, y) {
+    switchFocusGrid(x === 1 ? x : x - 1, y)
+}
+
+function switchNeighborGridRight(x, y) {
+    switchFocusGrid(x === 100 ? x : x + 1, y)
+}
+
+function setSheetBodyHeight() {
     var rect = sheetContent[0].getBoundingClientRect()
     var pageHeight = $(window).height()
     var height = pageHeight - 30 - rect.top
@@ -27,13 +106,51 @@ function setSheetBodyHeight(sideBar, sheetContent) {
     sideBar.css("height", "" + (height + 21) + "px")
 }
 
-function setSheetBodySizeListener(sideBar, sheetContent) {
+function initRowNumber(rowCount) {
+    for (var i = 1; i <= rowCount; ++i) {
+        var grid = document.createElement("div")
+        grid.id = "r" + i
+        grid.innerHTML = i
+        rowNumber.append(grid)
+    }
+}
+
+function initSheetHeader(columnCount) {
+    for (var i = 1; i <= columnCount; ++i) {
+        var grid = document.createElement("div")
+        grid.id = "c" + i
+        grid.innerHTML = getColumnName(i)
+        sheetHeader.append(grid)
+    }
+}
+
+function initSheetContent(columnCount, rowCount) {
+    for (var y = 1; y <= rowCount; ++y) {
+        var row = document.createElement("div")
+        row.className = "gridRow"
+        for (var x = 1; x <= columnCount; ++x) {
+            var grid = document.createElement("div")
+            grid.xIndex = x
+            grid.innerHTML = getGridValue(x, y)
+            row.appendChild(grid)
+        }
+        row.yIndex = y
+        row.appendChild(grid)
+        sheetContent.append(row)
+    }
+}
+
+function initFocusGrid() {
+    switchFocusGrid(focusX, focusY)
+}
+
+function addSheetBodyResizeListener(sheetContent) {
     $(window).resize(function() {
-        setSheetBodyHeight(sideBar, sheetContent)
+        setSheetBodyHeight()
     })
 }
 
-function setSheetContentScrollListener(rowNumber, sheetHeader, sheetContent) {
+function addSheetContentScrollListener() {
     var scrollTop = sheetContent.scrollTop()
     var scrollLeft = sheetContent.scrollLeft()
 
@@ -57,52 +174,57 @@ function setSheetContentScrollListener(rowNumber, sheetHeader, sheetContent) {
     })
 }
 
-function initSheetBody() {
-    var sideBar = $("#sideBar")
-    var rowNumber = $("#rowNumber")
-    var sheetHeader = $("#sheetHeader")
-    var sheetContent = $("#sheetContent")
-    setSheetBodyHeight(sideBar, sheetContent)
-    setSheetBodySizeListener(sideBar, sheetContent)
-    setSheetContentScrollListener(rowNumber, sheetHeader, sheetContent)
+function addSheetContentClickListener() {
+    $(".gridRow >div").click(function() {
+        var grid = $(this)
+        var x = grid[0].xIndex
+        var y = grid.parent()[0].yIndex
+        switchFocusGrid(x, y)
+    })
 }
 
-
-function initRowNumber(rowCount) {
-    var sheetHeader = $("#rowNumber")
-    for (var i = 1; i <= rowCount; ++i) {
-        var grid = document.createElement("div")
-        grid.innerHTML = i
-        sheetHeader.append(grid)
-    }
-}
-
-function initSheetHeader(columnCount) {
-    var sheetHeader = $("#sheetHeader")
-    for (var i = 1; i <= columnCount; ++i) {
-        var grid = document.createElement("div")
-        grid.innerHTML = getColumnName(i)
-        sheetHeader.append(grid)
-    }
-}
-
-function initSheetContent(columnCount, rowCount) {
-    var sheetContent = $("#sheetContent")
-    for (var i = 1; i <= rowCount; ++i) {
-        var row = document.createElement("div")
-        row.className = "gridRow"
-        for (var j = 1; j <= columnCount; ++j) {
-            var grid = document.createElement("div")
-            row.appendChild(grid)
+function addInputListener() {
+    gridValue.keydown(function(event) {
+        if (event.keyCode === 37 ) {
+            switchNeighborGridLeft(focusX, focusY)
+        } else if (event.keyCode === 38 ) {
+            switchNeighborGridUp(focusX, focusY)
+        } else if (event.keyCode === 39 ) {
+            switchNeighborGridRight(focusX, focusY)
+        } else if (event.keyCode === 40 || event.keyCode === 13 ) {
+            switchNeighborGridDown(focusX, focusY)
         }
-        row.appendChild(grid)
-        sheetContent.append(row)
-    }
+    })
 }
 
-function createSpreadSheet(columnCount, rowCount) {
-    initSheetBody()
+function addRefreshButtonClickListener() {
+    $("#refreshButton").click(function(event) {
+        sheetContent.empty()
+        alert("Sheet already destroyed, click OK to rebuild.")
+        initSheetBody(100, 100)
+    })
+}
+
+function initSheetBody(columnCount, rowCount) {
+    gridName = $("#gridName")
+    gridValue = $("#gridValue")
+    sideBar = $("#sideBar")
+    rowNumber = $("#rowNumber")
+    sheetHeader = $("#sheetHeader")
+    sheetContent = $("#sheetContent")
+
+    setSheetBodyHeight()
     initRowNumber(rowCount)
     initSheetHeader(columnCount)
     initSheetContent(columnCount, rowCount)
+    initFocusGrid()
+    addSheetBodyResizeListener(sheetContent)
+    addSheetContentScrollListener(sheetHeader, sheetContent)
+    addSheetContentClickListener()
+    addInputListener()
+    addRefreshButtonClickListener()
+}
+
+function createSpreadSheet(columnCount, rowCount) {
+    initSheetBody(columnCount, rowCount)
 }
